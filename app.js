b2a7df6aa885729cf0a7647f3bc68e7839427112
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-05-01 00:31:06 +0930

    Recursive over questions. Arrays ready to create objects.

--- app.js
+++ app.js
@@ -4,15 +4,112 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+const questions = require("./index");
 const render = require("./lib/htmlRenderer");
 
+// empty arrays for manager, engineerS and internS
+const manager = [];
+const engineers = [];
+const interns = [] ;
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+
+function collectData() {
+	let i = 0;
+	let keepgoing = true;
+	
+	function askOneQuestion() {
+				
+		inquirer.prompt(questions.questions[i])
+			.then((answer) => {
+				
+
+				// check on which question we are
+				switch(i) {
+					case 0: // create manager
+						//manager.push(answer);
+						i++;	
+						break;
+					case 1:	// push to manager
+					case 2:	// push to manager
+					case 3:	// push to manager
+					case 4: // push to manager
+						manager.push(answer);
+						i++;	
+					  	break;			
+					case 5: // decide if we want to add a new member
+						switch(answer.role) {
+							case 'Engineer': // jump to question 6
+								i=6;
+								break;
+							case 'Intern':	// jump to question 10
+								i=10;
+								break;
+							default: // finish
+								keepgoing = false;
+								break;
+						}			
+						break;
+					case 6: // push to engineers
+					case 7: // push to engineers
+					case 8: // push to engineers
+						engineers.push(answer);
+						i++;	
+						break;						
+					case 9:	// push to engineers and move back to question 5
+						engineers.push(answer);
+						i=5;
+						break;
+					case 10: // push to interns
+					case 11: // push to interns
+					case 12: // push to interns
+						interns.push(answer);
+						i++;
+						break;
+					case questions.questions.length -1: //push to interns and move back to question 5
+						interns.push(answer);
+						i=5;
+						break;
+					default: // we need to do nothing
+					    break;
+				  }
+				  
+				if (keepgoing === true) {
+					// move on to the question pointed by index i
+					question = questions.questions[i];		
+					// recursive invoation 
+					askOneQuestion();
+				} else {
+					throw "Thank you good bye!!!";
+				}
+			})
+			.catch(error => {
+				if (error.includes("Thank you")) {
+					console.log(manager);
+					console.log(engineers);
+					console.log(interns);
+					console.log("INFO: " + error);   
+				} else {
+					console.log("ERROR: " + error);   
+				}
+				
+			})
+		}
+
+	// here we start 
+	askOneQuestion();
+}
+
+
+collectData();
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
--- index.js
+++ index.js
@@ -1,12 +1,208 @@
 const questions = [
+    {
+        type: 'input',
+        name: 'teamName',
+        message: "What is your team's name?: ",
+        validate: (input) => {
 
+            // validate input is not empty
+            if (input.match(/[a-z0-9]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and can only be letters and numbers!";            
+        }      
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your manager's name?: ",
+        validate: (input) => {
+
+            // validate input is not empty
+            if (input.match(/[a-z]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be letters only!";            
+        }   
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your manager's id?: ",
+        validate: (input) => {
+
+	        // must be a number
+            if (input.match(/[0-9]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be number!";            
+        } 
+    },
+	{
+        type: 'input',
+        name: 'email',
+        message: "What is your manager's email?: ",
+        validate: (input) => {
+
+	        // must be e-mail format bla@bla
+            if (input.match(/[a-z0-9]+@[a-z0-9.]+/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty and must be valid email address!";
+        }
+    },
+	{
+        type: 'input',
+        name: 'number',
+        message: "What is your manager's office number: ",
+        validate: (input) => {
+
+            // must be a number
+            if (input.match(/[0-9]/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty and must be number!";
+        }
+    },
+    {   
+        type: 'list',
+        name: 'role',
+        message: "What type of team member would you like to add?: ",
+	    choices: [ "Engineer", "Intern", "I don't want to add any more team members" ],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your engineer's name?: ",
+        validate: (input) => {
+
+            // validate input is not empty
+            if (input.match(/[a-z]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be letters only!";            
+        }   
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your engineer's id?: ",
+        validate: (input) => {
+
+	        // must be a number
+            if (input.match(/[0-9]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be number!";            
+        } 
+    },
+	{
+        type: 'input',
+        name: 'email',
+        message: "What is your engineer's email?: ",
+        validate: (input) => {
+
+	        // must be e-mail format bla@bla
+            if (input.match(/[a-z0-9]+@[a-z0-9.]+/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty and must be valid email address!";
+        }
+    },
+	{
+        type: 'input',
+        name: 'github',
+        message: "What is your engineer's GitHub username: ",
+        validate: (input) => {
+
+            // must be a number
+            if (input.match(/[0-9a-z]+/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty!";
+        }
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your intern's name?: ",
+        validate: (input) => {
+
+            // validate input is not empty
+            if (input.match(/[a-z]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be letters only!";            
+        }   
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your  intern's id?: ",
+        validate: (input) => {
+
+	        // must be a number
+            if (input.match(/[0-9]/gi)) { 
+                return true;
+            } 
+        
+            return "ERROR: must not be empty and must be number!";            
+        } 
+    },
+	{
+        type: 'input',
+        name: 'email',
+        message: "What is your  intern's email?: ",
+        validate: (input) => {
+
+	        // must be e-mail format bla@bla
+            if (input.match(/[a-z0-9]+@[a-z0-9.]+/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty and must be valid email address!";
+        }
+    },
+	{
+        type: 'input',
+        name: 'number',
+        message: "What is your  intern's school?: ",
+        validate: (input) => {
+
+            // must be a number
+            if (input.match(/[0-9a-z]+/gi)) {
+                return true;
+            }
+
+            return "ERROR: must not be empty!";
+        }
+    },
 ];
+
+
+
+
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
 
+   
 }
 
-init();
+
+module.exports = {
+    questions: questions
+
+}
